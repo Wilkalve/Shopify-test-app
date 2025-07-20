1f5759ca08b17722a8432bcc1c565ce908325682
Wilkalve <168154175+Wilkalve@users.noreply.github.com>
2025-07-20 19:23:20 -0400

    Test2

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -2,20 +2,34 @@ import { redirect } from "@remix-run/node";
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
-  
-  if (url.searchParams.get("shop")) {
-    throw redirect(`/auth?${url.searchParams.toString()}`);
+
+  const shop = url.searchParams.get("shop");
+  const host = url.searchParams.get("host");
+
+  if (shop && host) {
+    throw redirect(`/auth?shop=${shop}&host=${host}`);
+  }
+
+  if (shop) {
+    throw redirect(`/auth?shop=${shop}`);
   }
 
   return redirect("/auth");
 };
 
 export const action = async ({ request }) => {
-  const { shop } = await request.formData();
-  
+  const formData = await request.formData();
+
+  const shop = formData.get("shop");
+  const host = formData.get("host");
+
+  if (shop && host) {
+    throw redirect(`/auth?shop=${shop}&host=${host}`);
+  }
+
   if (shop) {
     throw redirect(`/auth?shop=${shop}`);
   }
-  
+
   return redirect("/auth");
 };
