c81fc0814e1e4477fd41f269f5a68f642d2d9889
Wilkalve <168154175+Wilkalve@users.noreply.github.com>
2025-07-20 21:01:33 -0400

    Test6

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -1,4 +1,4 @@
-import "@shopify/shopify-app-remix/adapters/vercel";
+import "@shopify/shopify-app-remix/adapters/node";
 import {
   ApiVersion,
   AppDistribution,
@@ -6,7 +6,7 @@ import {
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server.js";
-
+import { Buffer } from "buffer";
 
 console.log("SHOPIFY_APP_URL:", process.env.SHOPIFY_APP_URL);
 
@@ -15,7 +15,7 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.January25,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: "https://shopify-test-app-alpha.vercel.app",
+  appUrl: process.env.SHOPIFY_APP_URL || "http://localhost:3000",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
@@ -23,24 +23,34 @@ const shopify = shopifyApp({
     unstable_newEmbeddedAuthStrategy: true,
     removeRest: true,
   },
+
+  // handle custom domains
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
+
+  // redirect with host parameter
+  async afterAuth({ session, redirect }) {
+    const shop = session.shop;
+    const host = Buffer.from(`${shop}/admin`).toString("base64");
+
+    return redirect(`/?shop=${shop}&host=${host}`);
+  },
 });
 
 export default shopify;
+
 export const apiVersion = ApiVersion.January25;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
 export const login = shopify.login;
 export const registerWebhooks = shopify.registerWebhooks;
 export const sessionStorage = shopify.sessionStorage;
+
+// Optional security headers
 export function addDocumentResponseHeaders(request, responseHeaders) {
-
-
   responseHeaders.delete("X-Frame-Options");
 
-  // Explicitly allow Shopify to embed your app
   responseHeaders.set(
     "Content-Security-Policy",
     "frame-ancestors 'self' https://*.myshopify.com https://admin.shopify.com"
